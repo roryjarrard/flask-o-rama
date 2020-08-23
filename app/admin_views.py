18fec095974d45ca30c33d44dfaf5f6401dd67b6
Rory Jarrard <roryjarrard@gmail.com>
2020-08-22 23:06:51 -0600

    add nested templates and bootstrap

--- app/admin_views.py
+++ app/admin_views.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route('/admin/dashboard')
 def admin_dashboard():
     return render_template('/admin/dashboard.html')
+
+
+@app.route('/admin/profile')
+def admin_profile():
+    return render_template('/admin/profile.html')
