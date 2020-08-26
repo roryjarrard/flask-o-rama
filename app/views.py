f4c6b1c1a3f08eef16697f9248b07a45a8f83875
Rory Jarrard <roryjarrard@gmail.com>
2020-08-25 21:07:34 -0600

    add forms and dynamic routes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 from datetime import datetime
+
+users = {
+    "mitsuhiko": {
+        "name": "Armin Ronacher",
+        "bio": "Creatof of the Flask framework",
+        "twitter_handle": "@mitsuhiko"
+    },
+    "gvanrossum": {
+        "name": "Guido Van Rossum",
+        "bio": "Creator of the Python programming language",
+        "twitter_handle": "@gvanrossum"
+    },
+    "elonmusk": {
+        "name": "Elon Musk",
+        "bio": "technology entrepreneur, investor, and engineer",
+        "twitter_handle": "@elonmusk"
+    }
+}
 
 
 @app.route('/')
@@ -11,6 +29,36 @@ def index():
 @app.route('/about')
 def about():
     return render_template('public/about.html')
+
+
+@app.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        req = request.form
+
+        missing = list()
+
+        for k, v in req.items():
+            if v == "":
+                missing.append(k)
+
+        if missing:
+            feedback = f"Missing fields for {', '.join(missing)}"
+            return render_template('public/sign_up.html', feedback=feedback)
+
+        return redirect(request.url)
+
+    return render_template('public/sign_up.html')
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    user = None
+
+    if username in users:
+        user = users[username]
+
+    return render_template('public/profile.html', username=username, user=user)
 
 
 @app.route('/jinja')
