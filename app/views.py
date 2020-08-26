13141456d0798734d52327c77bb7e8520549f5da
Rory Jarrard <roryjarrard@gmail.com>
2020-08-25 21:29:17 -0600

    working with JSON

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, jsonify, make_response
 from datetime import datetime
 
 users = {
@@ -59,6 +59,25 @@ def profile(username):
         user = users[username]
 
     return render_template('public/profile.html', username=username, user=user)
+
+
+@app.route('/json', methods=['POST'])
+def json_example():
+    # validate the request body contains JSON
+    if request.is_json:
+        # parse the JSON into a Python dict
+        req = request.get_json()
+
+        response_body = {
+            "message": "JSON received!",
+            "sender": req.get("name")
+        }
+
+        res = make_response(jsonify(response_body), 200)
+
+        return res
+    else:
+        return make_response(jsonify({'message': 'Request body must be JSON'}), 400)
 
 
 @app.route('/jinja')
